b8b4b1b5edadf99261fcd7b6a4cb9d61bae0576a
Sudo-Self <sudo-self@proton.me>
2023-11-20 22:34:27 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -5,3 +5,15 @@
 		</NuxtLayout>
 	</div>
 </template>
+<template>
+  <div
+    :class="{
+      'theme-light': !darkMode,
+      'theme-dark': darkMode,
+    }"
+    class="h-screen bg-themeBackground p-5"
+  >
+    <h1 class="text-themeText">Nuxt 3 Tailwind Dark Mode Demo</h1>
+    <Toggle v-model="darkMode" off-label="Light" on-label="Dark" />
+  </div>
+</template>
