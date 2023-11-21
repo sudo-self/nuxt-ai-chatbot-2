e47325202b962b6a7f9aab4dc98a566af894d40e
Sudo-Self <sudo-self@proton.me>
2023-11-21 01:23:00 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./components/**/*.{js,vue,ts}',
-		'./layouts/**/*.vue',
-		'./pages/**/*.vue',
-		'./plugins/**/*.{js,ts}',
-		'./nuxt.config.{js,ts}',
-		'./app.vue'
-	],
-	theme: {
-		extend: {}
-	},
-	plugins: []
-};
-module.exports = {
   content: [
-    `components/**/*.{vue,js,ts}`,
-    `layouts/**/*.vue`,
-    `pages/**/*.vue`,
-    `app.vue`,
-    `plugins/**/*.{js,ts}`,
-    `nuxt.config.{js,ts}`,
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./content/**/*.{md,mdx}",
   ],
+  darkMode: ["class"],
   theme: {
-    extend: {
-      colors: {
-        themeBackground: 'var(--background)',
-        themeText: 'var(--text)',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 }
