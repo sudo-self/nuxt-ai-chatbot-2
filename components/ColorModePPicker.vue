68876a168e0aad60cf93b1b7360b22bb53e3ff92
Sudo-Self <sudo-self@proton.me>
2023-11-20 22:18:05 -0700

    Add files via upload

---
+++ components/ColorModePPicker.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    data() {
+      return {
+        colors: ['system', 'light', 'dark', 'sepia']
+      }
+    }
+  }
+</script>
+<li
+  v-for="color of colors"
+  :key="color"
+  @click="$colorMode.preference = color"
+></li>
