4d32d095e983d020f4660f63f4dd6dac1bd3170c
Sudo-Self <sudo-self@proton.me>
2023-11-20 22:36:30 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,3 +13,22 @@ module.exports = {
 	},
 	plugins: []
 };
+module.exports = {
+  content: [
+    `components/**/*.{vue,js,ts}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `app.vue`,
+    `plugins/**/*.{js,ts}`,
+    `nuxt.config.{js,ts}`,
+  ],
+  theme: {
+    extend: {
+      colors: {
+        themeBackground: 'var(--background)',
+        themeText: 'var(--text)',
+      },
+    },
+  },
+  plugins: [],
+}
