fb83089a72aea787f4b809fae946f6cc4f27b911
Sudo-Self <sudo-self@proton.me>
2023-11-20 22:35:14 -0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,11 +1,4 @@
 <template>
-	<div class="bg-gray-50">
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
-	</div>
-</template>
-<template>
   <div
     :class="{
       'theme-light': !darkMode,
@@ -17,3 +10,38 @@
     <Toggle v-model="darkMode" off-label="Light" on-label="Dark" />
   </div>
 </template>
+
+<script setup lang="ts">
+import Toggle from '@vueform/toggle'
+import { useState } from '#app'
+import { onMounted, watch } from '@vue/runtime-core'
+
+type Theme = 'light' | 'dark'
+
+const LOCAL_STORAGE_THEME_KEY = 'theme'
+
+const darkMode = useState('theme', () => false)
+
+const setTheme = (newTheme: Theme) => {
+  localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+  darkMode.value = newTheme === 'dark'
+}
+
+onMounted(() => {
+  const isDarkModePreferred = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+  const themeFromLocalStorage = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme
+
+  if (themeFromLocalStorage) {
+    setTheme(themeFromLocalStorage)
+  } else {
+    setTheme(isDarkModePreferred ? 'dark' : 'light')
+  }
+})
+
+watch(darkMode, (selected) => {
+  setTheme(selected ? 'dark' : 'light')
+})
+</script>
+
+<style src="@vueform/toggle/themes/default.css"></style>
